48e66e5c26ef845e796fefd0bdad4b8c70f2d8f0
Diana-Kaba <kabadiana327@gmail.com>
2023-03-11 11:13:53 +0200

    Add drawFrame

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let drawingCanvas;
 let context;
+let timer;
 let position_x = 10;
 let position_y = 0;
 
@@ -8,7 +9,7 @@ window.onload = function () {
   drawingCanvas = document.getElementById("ship");
   context = drawingCanvas.getContext("2d");
   // Обновляем холст через 0.02 секунды
-  setTimeout("drawFrame()", 20);
+  timer = setInterval(drawFrame, 20);
 };
 
 function drawFrame() {
@@ -18,34 +19,41 @@ function drawFrame() {
   context.beginPath();
   // Рисуем парус
   context.fillStyle = "#fc0";
-  context.moveTo(20, 70);
-  context.lineTo(60, 20);
-  context.lineTo(60, 70);
+  context.moveTo(position_x + 20, 70);
+  context.lineTo(position_x + 60, 20);
+  context.lineTo(position_x + 60, 70);
   context.fill();
   // Рисуем палубу
   context.beginPath();
   context.fillStyle = "#ccf";
-  context.moveTo(0, 70);
-  context.lineTo(30, 100);
-  context.lineTo(70, 100);
-  context.lineTo(100, 70);
+  context.moveTo(position_x, 70);
+  context.lineTo(position_x + 30, 100);
+  context.lineTo(position_x + 70, 100);
+  context.lineTo(position_x + 100, 70);
   context.fill();
   // Рисуем мачту
   context.beginPath();
   context.fillStyle = "#a60";
-  context.fillRect(60, 5, 5, 65);
+  context.fillRect(position_x + 60, 5, 5, 65);
   // Рисуем флаг
   context.beginPath();
   context.fillStyle = "#e49";
-  context.fillRect(40, 5, 20, 10);
+  context.fillRect(position_x + 40, 5, 20, 10);
   // Пишем название
   context.fillStyle = "#00f";
   context.font = "italic 20px sans-serif";
   context.textBaseline = "top";
-  context.fillText("SHIP", 25, 75);
+  context.fillText("SHIP", position_x + 25, 75);
 
   // Перемещаем кораблик вправо на 1 пиксель (где он будет прорисован в следующем кадре)
   position_x += 1;
   // Рисуем следующий кадр через 20 миллисекунд
-  setTimeout("drawFrame()", 20);
+
+  if (position_x <= 0) {
+    position_x = 0;
+    clearInterval(timer);
+  } else if (position_x + 100 >= drawingCanvas.width) {
+    position_x = drawingCanvas.width - 100;
+    clearInterval(timer);
+  }
 }
